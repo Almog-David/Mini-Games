bd6b738f6e8bec2d9c6e109aa361e7beb9291937
Almog-David <almog.david99@gmail.com>
2022-08-21 20:34:03 +0300

    Rock Paper Scissor complete with GUI - first improvement

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,51 +1,130 @@
 package RockPaperScissors;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Random;
-import java.util.Scanner;
 
-public class RockPaperScissors {
-    public static void main(String [] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Welcome to the Rock Paper Scissors Game! you know the rules..");
-            String[] moves = {"r", "p", "s"};
-            String computerMove = moves[new Random().nextInt(moves.length)];
-            String playerMove;
-            while (true) {
-                System.out.println("Enter your move: r - rock, p - paper, s - scissors");
-                playerMove = scanner.nextLine();
-                if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
-                    break;
-                }
-                System.out.println(playerMove + " is not a valid move! try again");
-            }
-            System.out.println("the computer move is " + computerMove);
-            if (computerMove.equals(playerMove)) {
-                System.out.println("It's a tie");
-            }
-            else if (playerMove.equals("r")) {
-                if (computerMove.equals("p")) {
-                    System.out.println("you lose!");
-                } else {
-                    System.out.println("you win!");
-                }
-            }
-            else if (playerMove.equals("p")) {
-                if (computerMove.equals("s")) {
-                    System.out.println("you lose!");
-                } else {
-                    System.out.println("you win!");
-                }
-            }
-            else {
-                if (computerMove.equals("r")) {
-                    System.out.println("you lose!");
-                } else {
-                    System.out.println("you win!");
-                }
-            }
-            System.out.println("play again? (y/n)");
-            String playAgain = scanner.nextLine();
-            if(!playAgain.equals("y")){ break;}
-        }
+public class RockPaperScissors extends JFrame {
+
+    RockPaperScissors() {
+        this.setTitle("Rock Paper Scissors");
+        this.setBounds(200, 200, 500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.WHITE);
+        this.setup();
+        this.setResizable(false);
+        this.setVisible(true);
     }
+
+    JLabel computerChoice,result;
+    ImageIcon image_rock, image_paper,image_scissors;
+    public void setup(){
+        image_rock = new ImageIcon(new ImageIcon("src/RockPaperScissors/Graphics/rock.png").getImage()
+                .getScaledInstance(80,80,Image.SCALE_SMOOTH));
+        image_paper= new ImageIcon(new ImageIcon("src/RockPaperScissors/Graphics/paper.png").getImage()
+                .getScaledInstance(80,80,Image.SCALE_SMOOTH));
+        image_scissors= new ImageIcon(new ImageIcon("src/RockPaperScissors/Graphics/scissor.png").getImage()
+                .getScaledInstance(80,80,Image.SCALE_SMOOTH));
+
+    computerChoice = new JLabel();
+    computerChoice.setHorizontalTextPosition(JLabel.CENTER);
+    computerChoice.setVerticalTextPosition(JLabel.BOTTOM);
+    computerChoice.setBounds(200,70,100,100);
+
+    JLabel label_rock = new JLabel();
+    label_rock.setText("Rock");
+    label_rock.setIcon(image_rock);
+    label_rock.setHorizontalTextPosition(JLabel.CENTER);
+    label_rock.setVerticalTextPosition(JLabel.BOTTOM);
+    label_rock.setBounds(100,275,100,100);
+
+    JLabel label_paper = new JLabel();
+    label_paper.setText("Paper");
+    label_paper.setIcon(image_paper);
+    label_paper.setHorizontalTextPosition(JLabel.CENTER);
+    label_paper.setVerticalTextPosition(JLabel.BOTTOM);
+    label_paper.setBounds(200,275,100,100);
+
+    JLabel label_scissors = new JLabel();
+    label_scissors.setText("Scissors");
+    label_scissors.setIcon(image_scissors);
+    label_scissors.setHorizontalTextPosition(JLabel.CENTER);
+    label_scissors.setVerticalTextPosition(JLabel.BOTTOM);
+    label_scissors.setBounds(300,275,100,100);
+
+    label_rock.addMouseListener((new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            calculate(label_rock.getText());
+        }
+    }));
+
+        label_paper.addMouseListener((new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                calculate(label_paper.getText());
+            }
+        }));
+
+        label_scissors.addMouseListener((new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                calculate(label_scissors.getText());
+            }
+        }));
+
+   result = new JLabel();
+   result.setFont(new Font("Serif",Font.PLAIN,17));
+   result.setBounds(200,370,100,100);
+
+   JLabel label_computer = new JLabel();
+   label_computer.setText("Computer");
+   label_computer.setBounds(210, 150,100,100);
+
+   JLabel label_VS = new JLabel();
+   label_VS.setText("VS");
+   label_VS.setBounds(230, 170,100,100);
+
+   JLabel label_player = new JLabel();
+   label_player.setText("Player");
+   label_player.setBounds(220, 190,100,100);
+
+    add(label_rock);
+    add(label_paper);
+    add(label_scissors);
+    add(computerChoice);
+    add(result);
+    add(label_computer);
+    add(label_VS);
+    add(label_player);
+    setLayout(null);
+    }
+
+    public void calculate(String playerMove) {
+        String res;
+        String[] moves = {"Rock", "Paper", "Scissors"};
+        String computerMove = moves[new Random().nextInt(moves.length)];
+        computerChoice.setText(computerMove);
+        if (computerMove.equals("Rock")) { computerChoice.setIcon(image_rock);}
+
+        else if (computerMove.equals("Paper")) { computerChoice.setIcon(image_paper);}
+
+        else { computerChoice.setIcon(image_scissors);}
+
+        if (computerMove.equals(playerMove)) {
+            res="IT'S A TIE";
+        }
+        else if (playerMove.equals("Rock")) {
+            res = (computerMove.equals("Paper")) ? "YOU LOSE!" : "YOU WIN!";}
+
+        else if (playerMove.equals("Paper")) {
+            res = (computerMove.equals("Scissors")) ? "YOU LOSE!" : "YOU WIN!";}
+
+        else {
+            res = (computerMove.equals("Rock")) ? "YOU LOSE!" : "YOU WIN!";}
+
+        result.setText(res);
+    }
+
+
+    public static void main(String [] args) {
+        new RockPaperScissors();
+   }
 }
--- src/Whack_A_Mole/Game.java
+++ src/Whack_A_Mole/Game.java
@@ -8,31 +8,6 @@ import javax.swing.border.EmptyBorder;
 import java.util.Objects;
 import java.util.Random;
 
-//import java.awt.Color;
-//import java.awt.Font;
-//        import java.awt.Image;
-//        import java.awt.Point;
-//        import java.awt.Toolkit;
-//        import java.awt.event.ActionEvent;
-//        import java.awt.event.ActionListener;
-//        import java.awt.event.MouseAdapter;
-//        import java.awt.event.MouseEvent;
-//        import java.io.BufferedReader;
-//        import java.io.BufferedWriter;
-//        import java.io.FileReader;
-//        import java.io.FileWriter;
-//        import java.io.IOException;
-//
-//        import javax.swing.ImageIcon;
-//        import javax.swing.JButton;
-//        import javax.swing.JFrame;
-//        import javax.swing.JLabel;
-//        import javax.swing.JOptionPane;
-//        import javax.swing.JPanel;
-//        import javax.swing.SwingConstants;
-//        import javax.swing.Timer;
-
-
 public class Game extends JFrame{
 
     private JPanel panel;
@@ -94,15 +69,13 @@ public class Game extends JFrame{
         lblTimeLeft.setText("30");
 
         clearBoard();
-
         saveHighscore();
     }
 
     private void pressedButton(int id){
         int val = board[id];
 
-        //if val is 1 = mole
-        //if val is 0 = empty hole
+        //if val is 1 = mole. if val is 0 = empty hole
         if(val==1){
             score++;
         }else{ //val==0
@@ -110,9 +83,7 @@ public class Game extends JFrame{
         }
 
         lblScore.setText("Score: " + score); //update the score
-
         clearBoard();
-
         genRandMole();
     }
 
@@ -155,7 +126,6 @@ public class Game extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit thr program while pressing X
         setBounds(100, 100, 608, 720); // bounds of the window
 
-        //JPanel contentPane = new JPanel();
         JPanel contentPane = new JPanel();
         contentPane.setBackground(new Color(0, 51, 0));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -190,86 +160,6 @@ public class Game extends JFrame{
             }
             y_coordinate-=132;
         }
-
-//        holes[0] = new JLabel("0");
-//        holes[0].setName("0");
-//        holes[0].setBounds(0, 396, 132, 132);
-//        panel.add(holes[0]);
-//
-//        holes[1] = new JLabel("1");
-//        holes[1].setName("1");
-//        holes[1].setBounds(132, 396, 132, 132);
-//        panel.add(holes[1]);
-//
-//        holes[2] = new JLabel("2");
-//        holes[2].setName("2");
-//        holes[2].setBounds(264, 396, 132, 132);
-//        panel.add(holes[2]);
-//
-//        holes[3] = new JLabel("3");
-//        holes[3].setName("3");
-//        holes[3].setBounds(396, 396, 132, 132);
-//        panel.add(holes[3]);
-//
-//        holes[4] = new JLabel("4");
-//        holes[4].setName("4");
-//        holes[4].setBounds(0, 264, 132, 132);
-//        panel.add(holes[4]);
-//
-//        holes[5] = new JLabel("5");
-//        holes[5].setName("5");
-//        holes[5].setBounds(132, 264, 132, 132);
-//        panel.add(holes[5]);
-//
-//        holes[6] = new JLabel("6");
-//        holes[6].setName("6");
-//        holes[6].setBounds(264, 264, 132, 132);
-//        panel.add(holes[6]);
-//
-//        holes[7] = new JLabel("7");
-//        holes[7].setName("7");
-//        holes[7].setBounds(396, 264, 132, 132);
-//        panel.add(holes[7]);
-//
-//        holes[8] = new JLabel("8");
-//        holes[8].setName("8");
-//        holes[8].setBounds(0, 132, 132, 132);
-//        panel.add(holes[8]);
-//
-//        holes[9] = new JLabel("9");
-//        holes[9].setName("9");
-//        holes[9].setBounds(132, 132, 132, 132);
-//        panel.add(holes[9]);
-//
-//        holes[10] = new JLabel("10");
-//        holes[10].setName("10");
-//        holes[10].setBounds(264, 132, 132, 132);
-//        panel.add(holes[10]);
-//
-//        holes[11] = new JLabel("11");
-//        holes[11].setName("11");
-//        holes[11].setBounds(396, 132, 132, 132);
-//        panel.add(holes[11]);
-//
-//        holes[12] = new JLabel("12");
-//        holes[12].setName("12");
-//        holes[12].setBounds(0, 0, 132, 132);
-//        panel.add(holes[12]);
-//
-//        holes[13] = new JLabel("13");
-//        holes[13].setName("13");
-//        holes[13].setBounds(132, 0, 132, 132);
-//        panel.add(holes[13]);
-//
-//        holes[14] = new JLabel("14");
-//        holes[14].setName("14");
-//        holes[14].setBounds(264, 0, 132, 132);
-//        panel.add(holes[14]);
-//
-//        holes[15] = new JLabel("15");
-//        holes[15].setName("15");
-//        holes[15].setBounds(396, 0, 132, 132);
-//        panel.add(holes[15]);
 
         lblScore = new JLabel("Score: 0");
         lblScore.setHorizontalAlignment(SwingConstants.TRAILING);
